527160a78ad76cb4911eec58dadd72f2c8ba4350
vinodkumar-dev <vinodkumar0464@gmail.com>
2020-08-12 12:38:37 +0530

    added sop

--- src/main/java/com/samp/controller/HomeController.java
+++ src/main/java/com/samp/controller/HomeController.java
@@ -18,6 +18,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		System.out.println("Home Page Requested, locale = " + locale);
+System.out.println("modification file");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
